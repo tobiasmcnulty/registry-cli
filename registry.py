067461789271b16a0ed5bc28217aa340613ce591
Tobias McNulty <tobias@caktusgroup.com>
2025-12-12 16:08:19 -0500

    Fix SyntaxWarning: invalid escape sequence '\d'
    registry.py:136: SyntaxWarning: invalid escape sequence '\d'

--- registry.py
+++ registry.py
@@ -133,7 +133,7 @@ def natural_keys(text):
     def __atoi(text):
         return int(text) if text.isdigit() else text
 
-    return [__atoi(c) for c in re.split('(\d+)', text)]
+    return [__atoi(c) for c in re.split(r'(\d+)', text)]
 
 
 def decode_base64(data):
